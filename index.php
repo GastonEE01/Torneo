b6cd042646f24ebe66b1596596d3f4e8bbbbd397
GastonEE01 <gestevez@alumno.unlam.edu.ar>
2025-01-02 15:36:51 -0300

    Estructura del mvc

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-//session_start();
-include_once("helper/ManejoSesiones.php");
+    //session_start();
+    include_once("helper/ManejoSesiones.php");
 
-include_once ("Configuration.php");
-$router = Configuration::getRouter();
+    include_once ("Configuration.php");
+   $router = Configuration::getRouter();
 //$router = new Router("UsuarioController", "vistaLogin");
 
-$controller = isset($_GET["controller"]) ? $_GET["controller"] : "" ;
-$action = isset($_GET["action"]) ? $_GET["action"] : "" ;
+    $controller = isset($_GET["controller"]) ? $_GET["controller"] : "" ;
+    $action = isset($_GET["action"]) ? $_GET["action"] : "" ;
 
-$router->route($controller, $action);
+    $router->route($controller, $action);
+
